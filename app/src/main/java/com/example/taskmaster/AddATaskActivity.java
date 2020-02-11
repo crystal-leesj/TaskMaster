b67c3b169971fc0ed1a30dedec6669ae2925e4cf
Crystal Soo Lee <soo.leesj@gmail.com>
2020-02-11 12:58:02 -0800

    added back button

--- app/src/main/java/com/example/taskmaster/AddATaskActivity.java
+++ app/src/main/java/com/example/taskmaster/AddATaskActivity.java
@@ -2,11 +2,15 @@ package com.example.taskmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class AddATaskActivity extends AppCompatActivity {
 
@@ -19,11 +23,13 @@ public class AddATaskActivity extends AppCompatActivity {
         submitATaskButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                EditText input = findViewById(R.id.taskTitleEditText);
-                String inputText = input.getText().toString() + " submitted!";
-
-                TextView item = AddATaskActivity.this.findViewById(R.id.submitText);
-                item.setText(inputText);
+                EditText inputTitle = findViewById(R.id.taskTitleEditText);
+                Context context = getApplicationContext();
+                CharSequence submitText = inputTitle.getText().toString() + " task is submitted!";
+                int duration = Toast.LENGTH_SHORT;
+                Toast toast = Toast.makeText(context, submitText, duration);
+                toast.show();
+                toast.setGravity(Gravity.TOP|Gravity.CENTER_HORIZONTAL, 0, 0);
             }
         });
 
--- app/src/main/java/com/example/taskmaster/AllTasksActivity.java
+++ app/src/main/java/com/example/taskmaster/AllTasksActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class AllTasksActivity extends AppCompatActivity {
 
@@ -13,14 +14,5 @@ public class AllTasksActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_tasks);
-
-        Button goToHomeButton = (Button) findViewById(R.id.goBackHomeButton);
-        goToHomeButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent goToMain =  new Intent(AllTasksActivity.this, MainActivity.class);
-                AllTasksActivity.this.startActivity(goToMain);
-            }
-        });
     }
 }
