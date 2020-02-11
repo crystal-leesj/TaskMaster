7a9f68969001ae5aee52c7720e841263d1728ba5
Crystal Soo Lee <soo.leesj@gmail.com>
2020-02-11 00:48:07 -0800

    completed the tasks

--- app/src/main/java/com/example/taskmaster/MainActivity.java
+++ app/src/main/java/com/example/taskmaster/MainActivity.java
@@ -15,23 +15,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        Button goToAddATaskButton = (Button) findViewById(R.id.addTaskButton);
-//        goToAddATaskButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent goToAddATask =  new Intent(MainActivity.this, AddATaskActivity.class);
-//                MainActivity.this.startActivity(goToAddATask);
-//            }
-//        });
-//
-//        Button goToAllTasksTaskButton = (Button) findViewById(R.id.allTasksButton);
-//        goToAllTasksTaskButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent goToAllTasks =  new Intent(MainActivity.this, AllTasksActivity.class);
-//                MainActivity.this.startActivity(goToAllTasks);
-//            }
-//        });
+        Button goToAddATaskButton = (Button) findViewById(R.id.addTaskButton);
+        goToAddATaskButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent goToAddATask =  new Intent(MainActivity.this, AddATaskActivity.class);
+                MainActivity.this.startActivity(goToAddATask);
+            }
+        });
+
+        Button goToAllTasksTaskButton = (Button) findViewById(R.id.allTasksButton);
+        goToAllTasksTaskButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent goToAllTasks =  new Intent(MainActivity.this, AllTasksActivity.class);
+                MainActivity.this.startActivity(goToAllTasks);
+            }
+        });
     }
 
 }
