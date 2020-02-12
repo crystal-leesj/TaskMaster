f189c2ec2fe1e2992dfdb49e1793af6f84d66930
Crystal Soo Lee <soo.leesj@gmail.com>
2020-02-12 12:50:25 -0800

    lab 27

--- app/src/main/java/com/example/taskmaster/MainActivity.java
+++ app/src/main/java/com/example/taskmaster/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.taskmaster;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceManager;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.prefs.PreferenceChangeEvent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.d(TAG, "we are onCreate");
 
+        // Go to Add task page
         Button goToAddATaskButton = (Button) findViewById(R.id.addTaskButton);
         goToAddATaskButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // Go to All tasks page
         Button goToAllTasksTaskButton = (Button) findViewById(R.id.allTasksButton);
         goToAllTasksTaskButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,7 +44,63 @@ public class MainActivity extends AppCompatActivity {
                 MainActivity.this.startActivity(goToAllTasks);
             }
         });
+
+        // Go to Setting page
+        Button goToSettingButton = (Button) findViewById(R.id.settingButton);
+        goToSettingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent goToSetting = new Intent(MainActivity.this, SettingActivity.class);
+                MainActivity.this.startActivity(goToSetting);
+            }
+        });
+
+
+        final Button goToTask1Button = (Button) findViewById(R.id.task1);
+        goToTask1Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String taskTitle = goToTask1Button.getText().toString();
+                Log.i(TAG, taskTitle);
+                saveTitleToSharePrefer(taskTitle);
+            }
+        });
+
+        final Button goToTask2Button = (Button) findViewById(R.id.task2);
+        goToTask2Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String taskTitle = goToTask2Button.getText().toString();
+                Log.i(TAG, taskTitle);
+                saveTitleToSharePrefer(taskTitle);
+            }
+        });
+
+        final Button goToTask3Button = (Button) findViewById(R.id.task3);
+        goToTask3Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String taskTitle = goToTask3Button.getText().toString();
+                Log.i(TAG, taskTitle);
+                saveTitleToSharePrefer(taskTitle);
+            }
+        });
     }
+
+    private void saveTitleToSharePrefer(String title) {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("taskTitle", title);
+        editor.commit();
+        Intent goToTaskDetail = new Intent(MainActivity.this, TaskDetailActivity.class);
+        MainActivity.this.startActivity(goToTaskDetail);
+    }
+
+//    public void viewATask(View view) {
+//        Button goToTask1Button = (Button) findViewById(R.id.task1);
+//        Log.i(TAG, goToTask1Button.getText().toString());
+//        this.finish();
+//    }
 
     @Override
     protected void onStart() {
@@ -48,6 +112,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         Log.i(TAG, "resumed");
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        String username = sharedPrefs.getString("username", "default username");
+
+        TextView usernameTextView = findViewById(R.id.usernameTextView);
+        usernameTextView.setText(username + "'s task");
     }
 
     @Override
